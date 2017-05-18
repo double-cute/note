19cc74576d97d5c5b36a21f4a18607b84db244b6
Peter <zilengshan_109@163.com>
2017-05-18 11:45:33 +0800

    update

--- java/集合/关联Map系列/Map.java
+++ java/集合/关联Map系列/Map.java
@@ -7,34 +7,33 @@ static <K, V> Map<K, V> singletonMap(K k, V v);
 int size();
 boolean isEmpty();
 boolean equals(Object o);
-int hashCode(); // sum: hash(k) ^ hash(v)
-
+int hashCode(); // sum: hash(key) ^ hash(value)
 
 // 3. add & modify
 
 /* force add */
-	// i. ov put(k, v): force
-V put(K k, V v);
-	// ii. kv2v put(k, kv2v): force, null > del
-default V compute(K k, BiFunction<? super K, ? super V, ? extends V> kv2v);
-	// iii. vv2v put(k, vv2v): force, null > del, vv2v = !ov?defv:vv2v
-default V merge(K k, V defv, BiFunction<? super V, ? super V, ? extends V> vv2v);
-
-/* no asso then add */
 	// i. ov put(k, v)
-default V putIfAbsent(K k, V v);
-	// ii. k2v put(k, k2v) else k2v = ov
-default V computeIfAbsent(K k, Function<? super K, ? extends V> k2v);
+	V put(K k, V v);
+	// ii. kv2v put(k, kv2v): null>del
+	default V compute(K k, BiFunction<? super K, ? super V, ? extends V> kv2v);
+	// iii. vv2v put(k, vv2v): null>del, vv2v = !ov?dv:f(ov,dv)
+	default V merge(K k, V dv, BiFunction<? super V, ? super V, ? extends V> vv2v);
 
 /* asso then replace */
-	// i. ov replace(k, v)
-default V replace(K k, V v);
+	// i. ov replace(k, v);
+	default V replace(K k, V v);
 	// ii. boolean replace(k, ov, nv);
-default boolean replace(K k, V ov, V nv); // has key then do
-	// iii. kv2v replace(k, kv2v): null > del, no-asso r null
-default V computeIfPresent(K k, BiFunction<? super K, ? super V, ? extends V> kv2v);
+	default boolean replace(K k, V ov, V nv);
+	// iii. kv2v replace(k, kv2v): null>del, no-asso r null
+	default V computeIfPresent(K k, BiFunction<? super K, ? super V, ? extends V> kv2v);
 	// iv. force: change the world
-default void replaceAll(BiFunction<? super K, ? super V, ? extends V> kv2v);
+	default void replaceAll(BiFunction(? super K, ? super V, ? extends V> kv2v);
+
+/* no-asso then add */
+	// i. ov put(k, v)
+	default V putIfAbsent(K k, V v);
+	// ii. k2v put(k, k2v), asso r ov
+	default V computeIfAbsent(K k, Function<? super K, ? extends V> k2v);
 
 
 // 4. remove: force
@@ -46,11 +45,11 @@ void clear();
 boolean containsKey(Object k);
 boolean containsValue(Object v);
 
-// 6. k2v
-V get(Object k);
-default getOrDefault(Object k, V defVal); // exclude:k-null
+// 6. k2v: force
+V get(K k);
+V getOrDefault(K k, V dv); // k must not in
 
-//7. set
+// 7. set
 Set<K> keySet();
 Collection<V> values();
 Set<Map.Entry<K, V>> entrySet();
@@ -58,7 +57,7 @@ Set<Map.Entry<K, V>> entrySet();
 // 8. entry
 K getKey();
 V getValue();
-V setValue(V v);
+V setValue();
 static <K, V> Comparator<Map.Entry<K, V>>
 	comparingByKey|Value([Comparator<? super K|V> cmp]);
 
