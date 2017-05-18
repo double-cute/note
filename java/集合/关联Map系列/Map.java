a8482a3f8473fbdd7548ac41003e41dc637a04d4
Peter <zilengshan_109@163.com>
2017-05-18 12:30:52 +0800

    add

--- java/集合/关联Map系列/Map.java
+++ java/集合/关联Map系列/Map.java
@@ -9,45 +9,46 @@ boolean isEmpty();
 boolean equals(Object o);
 int hashCode(); // sum: hash(key) ^ hash(value)
 
+
 // 3. add & modify
 
 /* force add */
-	// i. ov put(k, v)
+	// i. ov put(k, nv)
 	V put(K k, V v);
 	// ii. kv2v put(k, kv2v): null>del
-	default V compute(K k, BiFunction<? super K, ? super V, ? extends V> kv2v);
-	// iii. vv2v put(k, vv2v): null>del, vv2v = !ov?dv:f(ov,dv)
-	default V merge(K k, V dv, BiFunction<? super V, ? super V, ? extends V> vv2v);
+	V compute(K k, BiFunction<? super K, ? super V, ? extends V> kv2v);
+	// iii. vv2v put(k, vv2v): null>del, vv2v = !ov:dv:f(ov,dv)
+	V merge(K k, V dv, BiFunction<? super V, ? super V, ? extends V> vv2v);
 
-/* asso then replace */
-	// i. ov replace(k, v);
-	default V replace(K k, V v);
-	// ii. boolean replace(k, ov, nv);
+/* asso replace */
+	// i. ov replace(k, nv), no-asso none r ov(null)
+	default V replace(K k, V nv);
+	// ii. boolean replace(k, ov, nv)
 	default boolean replace(K k, V ov, V nv);
-	// iii. kv2v replace(k, kv2v): null>del, no-asso r null
-	default V computeIfPresent(K k, BiFunction<? super K, ? super V, ? extends V> kv2v);
+	// iii. kv2v replace(k, kv2v): null>del, no-asso none r ov(null)
+	default V putIfPresent(K k, BiFunction<? super K, ? super V, ? extends V> kv2v);
 	// iv. force: change the world
-	default void replaceAll(BiFunction(? super K, ? super V, ? extends V> kv2v);
+	default void replaceAll(BiFunction<? super K, ? super V, ? extends V> kv2v);
 
-/* no-asso then add */
-	// i. ov put(k, v)
+/* no-asso replace */
+	// i. ov put(k, v), asso none r ov
 	default V putIfAbsent(K k, V v);
-	// ii. k2v put(k, k2v), asso r ov
+	// ii. k2v put(k, k2v), asso none r ov
 	default V computeIfAbsent(K k, Function<? super K, ? extends V> k2v);
 
 
-// 4. remove: force
+// 4. remove
 V remove(Object k);
-default boolean remove(Object k, Object ov, Object nv);
+default boolean remove(Object k, Object ov);
 void clear();
 
-// 5. contains: force
+// 5. contain
 boolean containsKey(Object k);
 boolean containsValue(Object v);
 
-// 6. k2v: force
+// 6. k2v
 V get(K k);
-V getOrDefault(K k, V dv); // k must not in
+V getOrDefault(K k, V dv); // k must not in then r dv
 
 // 7. set
 Set<K> keySet();
@@ -57,9 +58,9 @@ Set<Map.Entry<K, V>> entrySet();
 // 8. entry
 K getKey();
 V getValue();
-V setValue();
+V setValue(V v);
 static <K, V> Comparator<Map.Entry<K, V>>
-	comparingByKey|Value([Comparator<? super K|V> cmp]);
+	comparingByKey|Value([Comparator<? super K|V>]);
 
 // 9. iter
 default void forEach(BiConsumer<? super K, ? super V> action);
