9e8bc09aa92c92d7332212ddf95e2a5d80abd6ca
double-cute <zilengshan_109@163.com>
2017-05-22 17:13:50 +0800

    add

--- redis/消息通知（进程间通信）/index/pro-con.sh
+++ redis/消息通知（进程间通信）/index/pro-con.sh
@@ -14,3 +14,19 @@ infinite_loop:
             task_queue_low
             0;
     excute($task)
+
+
+# 2. publish-subscribe
+    # publish
+int(rec_cli_num):
+    PUBLISH ch message
+    # subscribe
+enter_mode:
+    SUBSCRIBE ch1 ch2 ...
+str[]:
+    UNSUBSCRIBE [ch1 ch2 ...]
+    # psubscribe
+enter_mode:
+    PSUBSCRIBE pattern_ch1 pattern_ch2 ...
+str[]:
+    PUNSUBSCRIBE [pattern_ch1 pattern_ch2 ...]
