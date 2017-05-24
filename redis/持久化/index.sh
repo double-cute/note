28ec6870c18a1f45c2894df2d5530f654b55f39f
double-cute <zilengshan_109@163.com>
2017-05-24 15:52:55 +0800

    add

---
+++ redis/持久化/index.sh
@@ -0,0 +1,32 @@
+# RDB
+
+# basic config
+dir $dumpfile_aof_dir
+dbfilename $dumpfile_rename
+appendfilename $aof_rename
+rdbcompression yes|no
+
+# 1. syn
+stat:
+    save
+
+# 2. asyn
+    # 1. conf
+    save window_time_len(s) num_key_change
+    # 2. manual
+    stat(message):
+        bgSave
+    # 3. flushall
+    stat:
+        flushAll
+    # 4. replication
+    # 5. OS mem apply
+    $ sudo vim /etc/sysctl.conf # vm.overcommit_memory = 1
+    $ reboot | $ sudo sysctl vm.overcommit_memory=1
+
+# 3. get
+int(unix_time_stamp):
+    lastSave
+
+
+# AOF
